37806b080bc4b9ab42bbad51648fa67db06c8b4f
shubhambatra3019 <31996668+shubhambatra3019@users.noreply.github.com>
2018-03-06 00:58:20 -0500

    Initial Commit

---
+++ mac Organizer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mac Organizer
+//
+//  Created by Shubham Batra on 06/03/18.
+//  Copyright © 2018 Esper. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mac Organizer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mac Organizer
+//
+//  Created by Shubham Batra on 06/03/18.
+//  Copyright © 2018 Esper. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
